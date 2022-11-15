16c8a34c45a75fa2684b4f4467474201195d82b3
WidgetWolf <xmasukox@gmail.com>
2022-11-15 16:35:18 +0100

    fixed error as the and logo png was not added to the repository

--- twitter-challenge/src/App.tsx
+++ twitter-challenge/src/App.tsx
@@ -1,18 +1,14 @@
-import React from 'react';
-import logo from './and-logo.png';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        This is the AND Twitter App
-        </p>
-        <p>
-        Version 0.02
-        </p>
+        <p>This is the AND Twitter App</p>
+        <p>Version 0.02</p>
         <a
           className="App-link"
           href="https://reactjs.org"
